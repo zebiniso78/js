1a43750ad1021145142264ecdaaa4c3d8a0c6c6e
zebiniso78 <murodilovnaz@gmail.com>
2021-07-07 13:55:06 -0700

    Anorther file

---
+++ js.6-dars/js/main.js
@@ -0,0 +1,41 @@
+//var a= prompt("Birinchi raqamni kiriting: ", 0);
+//var b= prompt("Ikkinchi raqamni kiriting: ", 1);
+
+//var x=Number(a)
+// var y=Number(b)
+
+// console.log(x+y);
+// console.log(typeof NaN) // identifies type of entered value
+// console.log(x%y)// 9%4=1
+// console.log(x/y)// 9/4=2.25
+
+// var number =5
+// number = number +1
+// ++number
+// console.log(number);
+
+// if(x===5) {
+//     console.log("ha bu besh raqami edi")
+// }
+// else {
+//     console.log("bu boshqa raqam edi")
+// }
+
+// if(x>5) {
+//     console.log("raqam 5dan katta")
+// }
+// else {
+//     console.log("raqam 5 dan kichik")
+// }
+
+var a= prompt("Birinchi raqamni kiriting: ", 5);
+var b= prompt("Ikkinchi raqamni kiriting: ", 7);
+var c= prompt("Ikkala raqam yig'indisini kiriting: ", 12);
+var result= prompt(`${4}+${5}=`)
+
+if(c===a+b) {
+    console.log("Kiritilgan natija to'g'ri")
+}
+else {
+    console.log("Kiritilgan natija noto'g'ri")
+}
